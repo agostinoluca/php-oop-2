bbeb6da615684194e1b6819d71f153b3f4e4416e
agostinoluca <agostino.luca89@gmail.com>
2024-03-20 19:56:22 +0100

    add Class Product

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
+class Product
+{
+    public function __construct(protected string $productName, protected int $price, protected string $image, protected string $productCategory, protected string $petsCategory)
+    {
+        $this->productName = $productName;
+        $this->price = $price;
+        $this->image = $image;
+        $this->productCategory = $productCategory;
+        $this->petsCategory = $petsCategory;
+    }
+};
+$classTest = new Product('Cuccia', 122, 99, 'Utility', 'Dog');
+var_dump($classTest);
+
+
 require_once __DIR__ . '/app/layout/head.php';
 ?>
 
